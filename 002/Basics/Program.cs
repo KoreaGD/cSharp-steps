12940d37447f329891aa51a85016b6a62f612cce
Everton <evertontrajoli@gmail.com>
2020-10-10 22:59:21 -0400

    Basics

---
+++ 002/Basics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Outputs a carriage-return
+            Console.WriteLine();
+            // Outputs the greeting and a carriage-return
+            Console.WriteLine("Hello CSharp, my name is Everton!");
+            // Outputs a formated number and date a carriage-return
+            Console.WriteLine("Temperature on {0:D} is {1}°C", DateTime.Today, 23);
+
+        }
+    }
+}
