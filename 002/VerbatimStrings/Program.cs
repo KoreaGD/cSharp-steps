b60affc3f5bbe1db06558a198cc4db2cf42f9143
Everton <evertontrajoli@gmail.com>
2020-10-14 23:01:46 -0400

    Verbatim Strings

---
+++ 002/VerbatimStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VerbatimStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullNameWithTabSeparator = "Bob\tSmith";
+            string filePath = @"\home\pi\";
+
+            Console.WriteLine(filePath);
+        }
+    }
+}
