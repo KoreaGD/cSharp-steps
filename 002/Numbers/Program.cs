f998e64ac2a2b10bb7ad8a4aa26d4d39f2204b1f
Everton <evertontrajoli@gmail.com>
2020-10-14 22:21:55 -0400

    Numbers

---
+++ 002/Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         // Unsingmed integer means positive whole number.
+         // Including 0   
+         uint naturalNumber =  23;
+         // integer means negative or, positove whole number
+         // Including 0
+         int integerNumber = -23;
+         // float means single precision floating points.
+         // F suffix makes it a float literal.
+         float realNumber = 2.3F;
+         // double means double-precision floating point double anotherRealNumber = 2.3; //Double Literal.
+        }
+    }
+}
