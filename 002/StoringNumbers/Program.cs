fbabdaee9c36c0f258e6cd77237b64836b4cf6a4
Everton <evertontrajoli@gmail.com>
2020-10-14 23:13:38 -0400

    Storing Numbers

---
+++ 002/StoringNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StoringNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // usinged integer means positive whole number ( including 0)
+            uint naturalNumber = 23;
+
+            // integer means negative or positive whole number ( including 0)
+            int integer = -23;
+
+            // float means single-precisionn floating point
+            // F suffix makes it a float literal
+            float realNumber = 2.3F;
+
+            // double means double-precision floating point
+            double anotherRealNumber = 2.3;// double litral
+            
+        }
+    }
+}
