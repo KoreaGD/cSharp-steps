f421723a02a0a7f0b4db25f353c5cbbaea8e7feb
Everton <evertontrajoli@gmail.com>
2020-10-15 18:34:07 -0400

    Comparing Decimal with Double

---
+++ 002/ComparingTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ComparingTypes {
+    class Program {
+        static void Main (string[] args) {
+            double a = 0.1;
+            double b = 0.2;
+
+            if (a + b == 0.3) {
+                Console.WriteLine ($"{a} + {b} equals 0.3");
+            } else {
+                Console.WriteLine ($"{a} + {b} does NOT equal 0.3");
+            }
+
+            Console.WriteLine ("Using decimals");
+
+            decimal c = 0.1M; // M suffix means a decimal literal value
+            decimal d = 0.2M;
+
+            if (c + d == 0.3M) {
+                Console.WriteLine ($"{c}+{d} equals 0.3");
+            } else {
+                Console.WriteLine ($"{c}+{d} not eaqual 0.3");
+            }
+        }
+    }
+}
