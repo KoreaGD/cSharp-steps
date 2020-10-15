d0dbbff20190393f53484f130cc9b35424d9286e
Everton <evertontrajoli@gmail.com>
2020-10-14 23:49:17 -0400

    Exploring Number sizes

---
+++ 002/ExploringNumberSizes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExploringNumberSizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:NO} to {int.MaxValue:NO}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:NO} to {double.MaxValue:NO}.");
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:NO} to {decimal.MaxValue:NO}.");
+        }
+    }
+}
