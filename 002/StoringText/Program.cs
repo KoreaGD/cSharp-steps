e783451b437fa6d6db83336d190e20ed4d6872f4
Everton <evertontrajoli@gmail.com>
2020-10-14 22:59:30 -0400

    Storing Text

--- 002/StoringText/Program.cs
+++ 002/StoringText/Program.cs
@@ -3,18 +3,19 @@
 namespace StoringText {
     class Program {
         static void Main (string[] args) {
-            // Char uses single Quote '
+                // Char uses single Quote '
             char letter = 'A'; //assing literal characters
             char digit = '1';
             char symbol = '$';
-            char userChoice = GetKeystroke (); //assinging from a function
-            // String uses double Quotes "
+            // char userChoice = GetKeystroke (); //assinging from a function
+
+                // String uses double Quotes "
             string firstName = "Everton"; // Assinging literal strings
             string lastName = "Trajano";
             string phoneNumber = "(55) 12345-6789";
 
-            // Assinging a string returned from a function call
-            String address = GetAddressFromDatabase(id: 563);
+                // Assinging a string returned from a function call
+            // String address = GetAddressFromDatabase(id: 563);
         }
     }
 }
